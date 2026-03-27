8e57ba9090e8e6bc2b8cca7bf042836344076a96
eomao <156084917+eomao@users.noreply.github.com>
2026-03-28 02:34:29 +0900

    Update util.h

--- code/1-2/util.h
+++ code/1-2/util.h
@@ -1,10 +1,5 @@
-#ifndef UTIL_H
-#define UTIL_H
-
 #include <stdio.h>
 
-static inline void logger(const char* message){
+static inline void logger(char* message){
     printf("log: %s\n", message);
 }
-
-#endif
