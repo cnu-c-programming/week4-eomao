5d3af79ea9318515dd6825dcd6f8038b7d616226
eomao <156084917+eomao@users.noreply.github.com>
2026-03-29 20:55:25 +0900

    Update 3-4.c

---
+++ code/3-4/3-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void inc(int* x){
+    if(isnull(x)){
+        return;
+    }
+    *x = *x + 1;
+    printf("%d\n", *x);
+}
+
+int main(){
+    int x = 3;
+
+    inc(&x);
+    inc(NULL);
+
+    return 0;
+}
