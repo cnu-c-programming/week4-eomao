431011790bbd546f624f594313076f8de15d92a3
eomao <156084917+eomao@users.noreply.github.com>
2026-03-29 22:29:36 +0900

    Update 4-3.c

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -4,14 +4,16 @@ int main() {
     int arr[] = {3, 7, 1, 9, 4, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    for(int i=0; i<(n-1); i++) {
+    for (int i = 0; i < (n - 1); i++) {
         void* addr_0 = &(arr[i]);
-        void* addr_1 = &(arr[i+1]);
+        void* addr_1 = &(arr[i + 1]);
         printf("address of arr[%d]: %p\n", i, addr_0);
-        printf("address of arr[%d+1]: %p\n", i, addr_1);
-        printf("(&(arr[%d]) - &(arr[%d+1])) = %llu\n", i, i+1,
+        printf("address of arr[%d]: %p\n", i + 1, addr_1);
+        printf("(&(arr[%d+1]) - &(arr[%d])) = %llu\n",
+               i, i,
                (unsigned long long)((unsigned char*)addr_1 - (unsigned char*)addr_0));
         printf("\n");
     }
+
     return 0;
 }
